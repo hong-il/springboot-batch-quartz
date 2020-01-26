38a50798ff436388aa9daea0886918540be3be76
KIM HONGIL <clroyan01@gmail.com>
2020-01-27 03:06:04 +0900

    Create batch quartz project

---
+++ src/test/java/boot/batch/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.batch.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
